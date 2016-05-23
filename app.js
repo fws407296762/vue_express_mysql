c7899f76c9c84adccd690c602423055afa959a71
fws407296762 <fws407296762@qq.com>
2016-05-23 23:50:40 +0800

    服务器搭建

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require("express");
-const ejs = require("ejs");
-
+const path = require("path");
+var ejs = require("ejs");
 var app = express();
-      
+
+app.set('views',path.join(__dirname,'/views'));
+app.set("view engine",'ejs');
+
 app.listen(3000);
 
-
 app.get("/",function(req,res){
-    res.send("OK");
+    res.render("index",{
+        title:"这个是首页"
+    })
 })
