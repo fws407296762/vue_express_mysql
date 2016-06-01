82d6452343ede958bbebf152a2a2f46fd2468fe2
fuws <fuws@cnns.net>
2016-06-01 12:49:05 +0800

    创建数据库

--- admin/db.js
+++ admin/db.js
@@ -2,16 +2,33 @@ const mysql = require("mysql");
 const config = require("../config");
 let options = config.Mysql;
 
+
 let connection = mysql.createConnection(options);
-// connection.connect();
+options.database = "vue";
+connection.connect();
 connection.on("error",function(err){
     console.log(err);
-    var query = connection.query("CREATE DATABASE IF NOT EXISTS vue",function(err,result){
-        console.log(err);
-    });
-})
-connection.end();
+});
 
+// let queryOptions = {
+//     sql:"CREATE DATABASE IF NOT EXISTS " + options.database +" default character set utf8 COLLATE utf8_general_ci;"
+// }
+
+let queryOptions = {
+   "sql":"SELECT SCHEMA_NAME FROM `information_schema`.`SCHEMATA` where SCHEMA_NAME='vue'"
+}
+let query = connection.query(queryOptions);
+query.on("error",function(err){
+    console.log(err);
+}).on("result",function(row){
+    console.log(row);
+}).on("fields",function(fields,index){
+    console.log(fields,index);
+}).on("end",function(){
+    
+});
+
+connection.end();
 
 module.exports = connection;
 
