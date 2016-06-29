f7b05d1cff7da557caa3c52826f7a6eed0bec195
fuws <fuws@cnns.net>
2016-06-29 09:52:09 +0800

    创建分页

---
+++ admin/vue/component/vuePagination.vue
@@ -0,0 +1,10 @@
+<template>
+    <ul class="pagination pagination-md pull-right">
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+    </ul>
+</template>
